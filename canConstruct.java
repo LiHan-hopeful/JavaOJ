4b97c9524a59479a204817c0dec2f34d3ff0a67d
LiHan-hopeful <1036177761@qq.com>
2020-03-27 11:32:13 +0800

    给定一个赎金信 (ransom) 字符串和一个杂志(magazine)字符串，判断第一个字符串ransom能不能由第二个字符串magazines里面的字符构成。如果可以构成，返回 true ；否则返回 false。
    (题目说明：为了不暴露赎金信字迹，要从杂志上搜索各个需要的字母，组成单词来表达意思。)
    
    注意：
    
    你可以假设两个字符串均只含有小写字母。
    
    来源：力扣（LeetCode）
    链接：https://leetcode-cn.com/problems/ransom-note
    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。

---
+++ canConstruct.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canConstruct(String ransom, String magazine) {
+        if (magazine.length() < ransom.length()) return false;
+        int caps[] = new int[26];
+        for (char c : ransom.toCharArray()) {
+            int index = magazine.indexOf(c, caps[c - 'a']);
+            if (index == -1)
+                return false;
+            caps[c - 97] = index + 1;
+        }
+        return true;
+    }
+}//给定一个赎金信 (ransom) 字符串和一个杂志(magazine)字符串，判断第一个字符串ransom能不能由第二个字符串magazines里面的字符构成。如果可以构成，返回 true ；否则返回 false。
+
