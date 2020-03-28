0398857bab90c255a58f70af28792a5d07f12e58
LiHan-hopeful <1036177761@qq.com>
2020-03-28 23:05:03 +0800

    给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 num1 成为一个有序数组。

---
+++ merge.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+    int p1 = m - 1;
+    int p2 = n - 1;
+    int p = m + n - 1;
+    while ((p1 >= 0) && (p2 >= 0)){
+          nums1[p--] = (nums1[p1] < nums2[p2]) ? nums2[p2--] : nums1[p1--];
+}
+    System.arraycopy(nums2, 0, nums1, 0, p2 + 1);
+    }
+}//给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 num1 成为一个有序数组。
