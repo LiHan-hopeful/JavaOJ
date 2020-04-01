b132170aba5aa13c2b75278e7baf3207701a1d6c
LiHan-hopeful <1036177761@qq.com>
2020-04-01 22:28:05 +0800

    给定一个非空数组，返回此数组中第三大的数。如果不存在，则返回数组中最大的数。要求算法时间复杂度必须是O(n)。

---
+++ thirdMax.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        TreeSet<Integer> tree = new TreeSet();
+        for(int i: nums){
+            tree.add(i);
+        }
+        if(tree.size() < 3) {
+            return tree.last();
+        }
+        else{
+            tree.remove(tree.last());
+            tree.remove(tree.last());
+            return tree.last();
+        }
+    }
+}
+
+
+
