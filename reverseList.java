3ad0693a79ebec06ddc42670036f7b37b1168005
LiHan-hopeful <1036177761@qq.com>
2020-04-10 10:58:27 +0800

    反转一个单链表。

---
+++ reverseList.java
@@ -0,0 +1,32 @@
+public class LinkedListTest {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+  //反转一个单链表。
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return head;
+        }
+        ListNode newHead = null;
+        ListNode cur = head;
+        ListNode prev = null;
+        while (cur != null) {
+            ListNode next = cur.next;
+            if (next == null) {
+                newHead = cur;
+            }
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return newHead;
+    }
+}
