5023b593eb2a3dc041ecfea8e126aa3634805a2b
LiHan-hopeful <1036177761@qq.com>
2020-04-11 22:23:37 +0800

    将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。

---
+++ mergeTwoLists.java
@@ -0,0 +1,39 @@
+public class LinkedListTest {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        ListNode newHead = new ListNode(-1);
+        ListNode newTail = newHead;
+        ListNode cur1 = l1;
+        ListNode cur2 = l2;
+        while (cur1 != null && cur2 != null) {
+            if (cur1.val < cur2.val) {
+                newTail.next = cur1;
+                newTail = newTail.next;
+                cur1 = cur1.next;
+            } else {
+                newTail.next = cur2;
+                newTail = newTail.next;
+                cur2 = cur1.next;
+            }
+        }
+        if (cur1 == null) {
+            newTail.next = cur2;
+        } else {
+            newTail.next = cur1;
+        }
+        return newHead.next;
+    }
+}
