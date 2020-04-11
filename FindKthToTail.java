fd1253d8ee23398769ce3d2e4246716e3f60d0d4
LiHan-hopeful <1036177761@qq.com>
2020-04-11 22:22:50 +0800

    输入一个链表，输出该链表中倒数第k个结点。

---
+++ FindKthToTail.java
@@ -0,0 +1,25 @@
+public class LinkedListTest {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+    public ListNode FindKthToTail(ListNode head, int k) {
+        if (head == null) {
+            return null;
+        }
+        int size = size(head);
+        if (k <= 0 || k > size) {
+            return null;
+        }
+        int steps = size - k;
+        ListNode cur = head;
+        for (int i = 0; i < steps; i++) {
+            cur = cur.next;
+        }
+        return cur;
+    }
+}
