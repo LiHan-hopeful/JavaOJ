cf013c5da1c2e6817889ae87147128ea88de3e0c
LiHan-hopeful <1036177761@qq.com>
2020-04-12 21:53:37 +0800

    在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，重复的结点不保留，返回链表头指针。

---
+++ deleteDuplication.java
@@ -0,0 +1,27 @@
+public class LinkedListTest {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+    public ListNode deleteDuplication(ListNode pHead) {
+        ListNode newHead = new ListNode(-1);
+        ListNode newTail = newHead;
+        ListNode cur = pHead;
+        while (cur != null) {
+            if (cur.next != null && cur.val == cur.next.val) {
+                while (cur.next != null && cur.val == cur.next.val) {
+                    cur = cur.next;
+                }
+                cur = cur.next;
+            } else {
+                newTail.next = new ListNode(cur.val);
+                cur = cur.next;
+            }
+        }
+        return newHead.next;
+    } 
+}
