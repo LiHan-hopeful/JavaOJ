8332c649f4265d96e59dbc88c068fc3c91407f62
LiHan-hopeful <1036177761@qq.com>
2020-04-18 23:38:05 +0800

    树的三种非递归遍历

---
+++ InterviewTree.java
@@ -0,0 +1,95 @@
+import java.util.Stack;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class InterviewTree {
+    public static TreeNode build() {
+        TreeNode a = new TreeNode(1);
+        TreeNode b = new TreeNode(2);
+        TreeNode c = new TreeNode(3);
+        TreeNode d = new TreeNode(4);
+        TreeNode e = new TreeNode(5);
+        TreeNode f = new TreeNode(6);
+        TreeNode g = new TreeNode(7);
+
+        a.left = b;
+        a.right = c;
+        b.left = d;
+        b.right = e;
+        e.left = g;
+        c.right = f;
+        return a;
+    }
+
+    public static void preOrderByLoop(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode top = stack.pop();
+            System.out.print(top.val + " ");
+            if (top.right != null) {
+                stack.push(top.right);
+            }
+            if (top.left != null) {
+                stack.push(top.left);
+            }
+        }
+    }
+
+    public static void inOrderByLoop(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (true) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            if (stack.isEmpty()) {
+                break;
+            }
+            TreeNode top = stack.pop();
+            System.out.print(top.val + " ");
+            cur = top.right;
+        }
+    }
+
+    public static void postOrderByLoop(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        TreeNode prev = null;
+        while (true) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            if (stack.isEmpty()) {
+                break;
+            }
+            TreeNode top = stack.peek();
+            if (top.right == null || prev == top.right) {
+                System.out.print(top.val + " ");
+                stack.pop();
+                prev = top; 
+            } else {
+                cur = top.right;
+            }
+        }
+    }
+}
