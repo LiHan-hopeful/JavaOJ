31253b7bbded19fcc09dee49cc20caeabd5bbd7d
LiHan-hopeful <1036177761@qq.com>
2020-05-10 22:13:14 +0800

    整数转罗马数字

---
+++ intToRoman.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String intToRoman(int num) {
+        StringBuilder ans = new StringBuilder();
+        String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};  
+        int[] arab = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};  
+        int index = 0;
+        while (num > 0) {
+            int count = num / arab[index];
+            while (count-- > 0) {
+                ans.append(roman[index]);
+            }
+            num %= arab[index];
+            index++;
+        }
+        return ans.toString();
+    }
+}
