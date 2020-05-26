b58d01626232d251c5feca25ae4ff83037c3eea8
LiHan-hopeful <1036177761@qq.com>
2020-05-26 23:39:10 +0800

    给定一个包含n+1个整数的数组 nums，其数字都在1到n 之间（包括1和n），可知至少存在一个重复的整数。假设只有一个重复的整数，找出这个重复的数。

---
+++ findDuplicate.java
@@ -0,0 +1,23 @@
+public class Solution {
+
+    public int findDuplicate(int[] nums) {
+        int len = nums.length;
+        int left = 1;
+        int right = len - 1;
+        while (left < right) {
+            int mid = (left + right) >>> 1;           
+            int cnt = 0;
+            for (int num : nums) {
+                if (num <= mid) {
+                    cnt += 1;
+                }
+            }
+            if (cnt > mid) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
