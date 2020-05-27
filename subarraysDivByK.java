7b9f1ab5a2ded2d4da4f3e81b8c9dbbe2253080f
LiHan-hopeful <1036177761@qq.com>
2020-05-27 17:23:46 +0800

    给定一个整数数组 A，返回其中元素之和可被 K 整除的（连续、非空）子数组的数目

---
+++ subarraysDivByK.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int subarraysDivByK(int[] A, int K) {
+        Map<Integer, Integer> record = new HashMap<>();
+        record.put(0, 1);
+        int sum = 0;
+        for (int elem: A) {
+            sum += elem;
+            int modulus = (sum % K + K) % K;
+            record.put(modulus, record.getOrDefault(modulus, 0) + 1);
+        }
+
+        int ans = 0;
+        for (Map.Entry<Integer, Integer> entry: record.entrySet()) {
+		    ans += entry.getValue() * (entry.getValue() - 1) / 2;
+		}
+        return ans;
+    }
+}
