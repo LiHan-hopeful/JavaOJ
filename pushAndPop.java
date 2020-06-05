03ed569e63bd4310dc6be1606bf611985e6e6655
LiHan-hopeful <1036177761@qq.com>
2020-06-05 21:45:16 +0800

    用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。

---
+++ pushAndPop.java
@@ -0,0 +1,24 @@
+import java.util.Stack;
+
+public class Solution {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+    
+    public void push(int node) {
+        stack1.push(node);
+    }
+    
+    public int pop() {
+    int result = 0;
+        if(stack2.size()!=0) {
+            result=stack2.pop();
+        }
+        else {
+            while(stack1.size()!=0) {
+                stack2.push(stack1.pop());
+            }
+            result=stack2.pop();
+        }
+        return result;
+    }
+}
