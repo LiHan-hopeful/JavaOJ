b96cc82410292ec55d8eac62c587d7bb2390a48e
LiHan-hopeful <1036177761@qq.com>
2020-06-07 21:41:02 +0800

    正整数A和正整数B的最小公倍数是指能被A和B整除的最小的正整数值，设计一个算法，求输入A和B的最小公倍数。

---
+++ getLCM.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class test {
+    public static int lcm(int m, int n) {
+        return (m * n) / gcd(m, n);
+    }
+
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = 0;
+        int b = 0;
+        while(scanner.hasNext()){
+             a = scanner.nextInt();
+             b = scanner.nextInt();
+        }
+        System.out.println(lcm(a, b));
+    }
+}
