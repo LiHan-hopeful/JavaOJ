8b85410710752d72a611dababdbea54c104b68d7
LiHan-hopeful <1036177761@qq.com>
2020-06-23 22:52:39 +0800

    链表面试题练习（删除给定值的节点）

---
+++ deleteTest.java
@@ -0,0 +1,27 @@
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        if (head == null) {
+            return null;
+        }
+        ListNode prev = head;
+        ListNode cur = head.next;
+        while(cur != null) {
+            if(cur.val == val) {
+                prev.next = cur.next;
+                cur = prev.next;
+            } else {
+                prev = cur;
+                cur = cur.next;
+            }
+        }
+        if(head.val == val) {
+            head = head.next;
+        }
+        return head;
+    }
+}
