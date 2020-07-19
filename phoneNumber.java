5e6bfc9579223db284a2fcca9e3d87ca2d340fa5
LiHan-hopeful <1036177761@qq.com>
2020-07-19 23:46:22 +0800

    上图是一个电话的九宫格，如你所见一个数字对应一些字母，因此在国外企业喜欢把电话号码设计成与自己公司名字相对应。例如公司的Help Desk号码是4357，因为4对应H、3对应E、5对应L、7对应P，因此4357就是HELP。同理，TUT-GLOP就代表888-4567、310-GINO代表310-4466。 NowCoder刚进入外企，并不习惯这样的命名方式，现在给你一串电话号码列表，请你帮他转换成数字形式的号码，并去除重复的部分。

---
+++ phoneNumber.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String symbol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        String number = "222333444555666777788899991234567890";
+        
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            int n = sc.nextInt();
+            ArrayList<String> list = new ArrayList<>();
+            for(int i = 0; i < n; i++) {
+                String str = sc.next();
+                str = str.replace("-", "");
+                String result = "";
+                for(int j = 0; j < 7; j++) {
+                    result += number.charAt(symbol.indexOf(str.charAt(j)+""));
+                }
+                result = result.substring(0, 3) + "-" + result.substring(3, 7);
+                if(!list.contains(result)) {
+                    list.add(result);
+                }
+            }
+            Collections.sort(list);
+            for(int j = 0; j < list.size(); j++) {
+                System.out.println(list.get(j));
+            }
+            System.out.println();
+        }
+    }
+}
