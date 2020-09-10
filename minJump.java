fbb4fc2ceb1e26ad9d49ddfb300d7c330505a31e
LiHan-hopeful <1036177761@qq.com>
2020-09-10 23:40:54 +0800

    为了给刷题的同学一些奖励，力扣团队引入了一个弹簧游戏机。游戏机由 N 个特殊弹簧排成一排，编号为 0 到 N-1。初始有一个小球在编号 0 的弹簧处。若小球在编号为 i 的弹簧处，通过按动弹簧，可以选择把小球向右弹射 jump[i] 的距离，或者向左弹射到任意左侧弹簧的位置。也就是说，在编号为 i 弹簧处按动弹簧，小球可以弹向 0 到 i-1 中任意弹簧或者 i+jump[i] 的弹簧（若 i+jump[i]>=N ，则表示小球弹出了机器）。小球位于编号 0 处的弹簧时不能再向左弹。
    为了获得奖励，你需要将小球弹出机器。请求出最少需要按动多少次弹簧，可以将小球从编号 0 弹簧弹出整个机器，即向右越过编号 N-1 的弹簧。
    
    来源：力扣（LeetCode）
    链接：https://leetcode-cn.com/problems/zui-xiao-tiao-yue-ci-shu
    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。

---
+++ minJump.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minJump(int[] jump) {
+        int[] dp = new int[jump.length];
+        dp[jump.length - 1] = 1;
+        for(int i = jump.length - 2; i > -1; --i){
+            dp[i] = jump[i] + i >= jump.length ? 1 : dp[jump[i] + i] + 1;
+            //遍历当前位置更新后影响到的后面的位置，只需要更新到dp[j] >= dp[i]+1即可
+            //如果遍历到某dp[j]<dp[i]+1就不需要向右遍历了,因为j到dp.length的值会被当前遍历到的dp[j]更新而不是dp[i]+1
+             for(int j = i + 1; j < dp.length && dp[j] >= dp[i] + 1; ++j){
+                dp[j] = dp[i] + 1;
+            }
+        }
+        return dp[0];
+    }
+}
+
